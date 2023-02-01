46364e2205e101846923b3f673e9a9ec023e92a0
ckakarla <ckakarla89@gmail.com>
2023-02-01 19:09:48 +0530

    We are doing this for Functions with Inputs Trail

--- special-vars.sh
+++ special-vars.sh
@@ -4,3 +4,13 @@ echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example() {
+  echo "following values from function"
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
