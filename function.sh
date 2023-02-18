51539ede2de90961ea9205cb7e01bd231fc2d3f0
ckakarla <ckakarla89@gmail.com>
2023-02-18 21:43:34 +0530

    We are doing this for learning shell script for Functions

---
+++ function.sh
@@ -0,0 +1,16 @@
+# Functions are like variables
+# Variable - When we assign a name to a set of data then it is a variable.
+# Function - When we assign a name to a set of commands then it is function.
+
+
+# Function Declare
+example() {
+  echo I am a example function
+  echo a - $a
+  b=20
+}
+
+## Call a function
+a=10
+example
+echo b - $b
